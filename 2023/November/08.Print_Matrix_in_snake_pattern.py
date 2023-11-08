5ad951e874aa7617882d3065dc8f6a81f58a3087
Anonymo Winter <131743419+Anonymo-Winter@users.noreply.github.com>
2023-11-08 16:33:05 +0530

    Nov 08 2023
    Given a matrix of size N x N. Print the elements of the matrix in the snake like pattern depicted below.
    Example 1:
    Input:
    N = 3
    matrix[][] = {{45, 48, 54},
                 {21, 89, 87}
                 {70, 78, 15}}
    Output:
    45 48 54 87 89 21 70 78 15
    Explanation:
    Matrix is as below:
    45 48 54
    21 89 87
    70 78 15
    Printing it in snake pattern will lead to
    the output as 45 48 54 87 89 21 70 78 15.
    Example 2:
    
    Input:
    N = 2
    matrix[][] = {{1, 2},
                  {3, 4}}
    Output:
    1 2 4 3
    Explanation:
    Matrix is as below:
    1 2
    3 4
    Printing it in snake pattern will
    give output as 1 2 4 3.

---
+++ 2023/November/08.Print_Matrix_in_snake_pattern.py
@@ -0,0 +1,46 @@
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return list of integers visited in snake pattern in matrix.
+        n=len(matrix)
+        list=[]
+        row=0
+        
+        while(row<n):
+            if(row%2==0):
+                for i in range(0,n):
+                    list.append(matrix[row][i])
+            else:
+                for i in range(n-1,-1,-1):
+                    list.append(matrix[row][i])
+            
+            row+=1
+        return list
+                
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        k = 0
+        matrix =[]
+        for i in range(n):
+            row=[]
+            for j in range(n):
+                row.append(values[k])
+                k+=1
+            matrix.append(row)
+        obj = Solution()
+        ans = obj.snakePattern(matrix)
+        for i in ans:
+            print(i,end=" ")
+        print()
+
+
+# } Driver Code Ends
